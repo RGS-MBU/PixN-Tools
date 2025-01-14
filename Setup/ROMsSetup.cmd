8a84ade48964c200728aedac8fd2757727012e95
Rapster-UK <rapsters.tech.hangout@gmail.com>
2025-01-14 17:12:23 +0000

    Update ROMsSetup

--- Setup/ROMsSetup.cmd
+++ Setup/ROMsSetup.cmd
@@ -15,6 +15,8 @@ ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
 
 REM CSCRIPT //nologo "%TempVBSFile%"
 
+:START
+CLS
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 echo                             ___
@@ -47,7 +49,7 @@ rem Change the current directory to the directory where the script is located
 cd /d "%~dp0"
 
 :inputLoop
-rem Prompt the user to enter the drive letter for RetroBat
+rem Prompt the user to enter the drive letter for the ROMs
 echo.
 echo This setup will install all or selective RomPacks.
 echo.
@@ -73,13 +75,16 @@ if exist "%ROMsPath%\*" (
     choice /C YN /M "ROMs folder already exists on the specified drive. Do you want to overwrite it"
     if errorlevel 2 (
         echo.
-		echo Stopping the extraction.
+		echo Returning to the start...
+		timeout /t 3 >nul
+		echo.
+		goto START
     ) else (
         echo.
 		echo Extracting ROMs to %ROMsPath%...
 	)
 )
-echo.
+
 dir "..\ROMs Starter Pack\*.7z" > nul 2>&1
 if %errorlevel% equ 0 (
     choice /C YN /M "Do you want to extract ALL RomPacks"
@@ -152,6 +157,221 @@ if %errorlevel% equ 0 (
     echo No .7z archives found in RomPacks folder.
     echo Use the checkbox selection inside the torrent content in order to download more RomPacks.
 )
+
+cls
+
+:inputLoop2
+echo.
+echo ..............................................................
+echo ....This section allows you to extract the 'Batocera Only'....
+echo .................and 'RetroBat Only' RomPacks.................
+echo ..............................................................
+echo.
+
+choice /C YN /M "Do you want to extract the 'Batocera Only' ROMs..."
+    if errorlevel 2 (
+        echo.
+		goto RB-Roms
+    ) else (
+        echo.
+	)
+
+:Bat-ROMs
+rem Set the ROMsPath to the specified drive
+set "ROMsPath=%driveLetter%:\PixN-ROMs-Batocera"
+
+rem Check if ROMs folder already exists
+if exist "%ROMsPath%\*" (
+    choice /C YN /M "ROMs folder already exists on the specified drive. Do you want to overwrite it"
+    if errorlevel 2 (
+        echo.
+		echo Returning to the start of this section...
+		timeout /t 3 >nul
+		echo.
+		goto inputLoop2
+    ) else (
+        echo.
+		echo Extracting ROMs to %ROMsPath%...
+	)
+)
+
+dir "..\ROMs Starter Pack\Batocera Only\*.7z" > nul 2>&1
+if %errorlevel% equ 0 (
+    choice /C YN /M "Do you want to extract ALL the Batocera RomPacks"
+    if errorlevel 2 (
+        echo.
+		echo Entering individual RomPack mode...
+		echo.
+        for %%f in ("..\ROMs Starter Pack\Batocera Only\*.7z") do (
+            choice /C YN /M "Do you want to extract %%~nxf"
+            if errorlevel 2 (
+                echo Skipping extraction of %%~nxf.
+            ) else (
+                rem Check if RomPack archive has already been extracted
+                if exist "%ROMsPath%\roms\%%~nf" (
+                    choice /C YN /M "RomPack %%~nxf already exists. Do you want to overwrite it"
+                    if errorlevel 2 (
+                        echo Skipping extraction of %%~nxf.
+                    ) else (
+                        echo Extracting %%~nxf to %ROMsPath%\roms...
+                        7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                        if errorlevel 1 (
+                            echo Error: Extraction of %%~nxf failed.
+                        ) else (
+                            echo Extraction of %%~nxf completed.
+                        )
+                    )
+                ) else (
+                    echo.
+					echo Extracting %%~nxf to %ROMsPath%\roms...
+                    7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                    if errorlevel 1 (
+                        echo Error: Extraction of %%~nxf failed.
+                    ) else (
+                        echo Extraction of %%~nxf completed.
+                    )
+                )
+            )
+        )
+    ) else (
+        echo.
+		echo Extracting all RomPacks...
+        echo.
+		for %%f in ("..\ROMs Starter Pack\Batocera Only\*.7z") do (
+            rem Check if RomPack archive has already been extracted
+            if exist "%ROMsPath%\roms\%%~nf" (
+                choice /C YN /M "RomPack %%~nxf already exists. Do you want to overwrite it"
+                if errorlevel 2 (
+                    echo Skipping extraction of %%~nxf.
+                ) else (
+                    echo Extracting %%~nxf to %ROMsPath%\roms...
+                    7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                    if errorlevel 1 (
+                        echo Error: Extraction of %%~nxf failed.
+                    ) else (
+                        echo Extraction of %%~nxf completed.
+                    )
+                )
+            ) else (
+                echo Extracting %%~nxf to %ROMsPath%\roms...
+                7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                if errorlevel 1 (
+                    echo Error: Extraction of %%~nxf failed.
+                ) else (
+                    echo Extraction of %%~nxf completed.
+                )
+            )
+        )
+    )
+) else (
+    echo No .7z archives found in 'Batocera Only' folder.
+    echo Use the checkbox selection inside the torrent content in order to download these RomPacks.
+)
+
+
+
+:RB-Roms
+echo.
+choice /C YN /M "Do you want to extract the 'RetroBat Only' ROMs..."
+    if errorlevel 2 (
+        echo.
+		goto END
+    ) else (
+        echo.
+	)
+
+rem Set the ROMsPath to the specified drive
+set "ROMsPath=%driveLetter%:\PixN-ROMs-RetroBat"
+
+rem Check if ROMs folder already exists
+if exist "%ROMsPath%\*" (
+    choice /C YN /M "ROMs folder already exists on the specified drive. Do you want to overwrite it"
+    if errorlevel 2 (
+        echo.
+		echo Returning to the start of this section...
+		timeout /t 3 >nul
+		echo.
+		goto RB-Roms
+    ) else (
+        echo.
+		echo Extracting ROMs to %ROMsPath%...
+	)
+)
+
+dir "..\ROMs Starter Pack\RetroBat Only\*.7z" > nul 2>&1
+if %errorlevel% equ 0 (
+    choice /C YN /M "Do you want to extract ALL the RetroBat RomPacks"
+    if errorlevel 2 (
+        echo.
+		echo Entering individual RomPack mode...
+		echo.
+        for %%f in ("..\ROMs Starter Pack\RetroBat Only\*.7z") do (
+            choice /C YN /M "Do you want to extract %%~nxf"
+            if errorlevel 2 (
+                echo Skipping extraction of %%~nxf.
+            ) else (
+                rem Check if RomPack archive has already been extracted
+                if exist "%ROMsPath%\roms\%%~nf" (
+                    choice /C YN /M "RomPack %%~nxf already exists. Do you want to overwrite it"
+                    if errorlevel 2 (
+                        echo Skipping extraction of %%~nxf.
+                    ) else (
+                        echo Extracting %%~nxf to %ROMsPath%\roms...
+                        7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                        if errorlevel 1 (
+                            echo Error: Extraction of %%~nxf failed.
+                        ) else (
+                            echo Extraction of %%~nxf completed.
+                        )
+                    )
+                ) else (
+                    echo.
+					echo Extracting %%~nxf to %ROMsPath%\roms...
+                    7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                    if errorlevel 1 (
+                        echo Error: Extraction of %%~nxf failed.
+                    ) else (
+                        echo Extraction of %%~nxf completed.
+                    )
+                )
+            )
+        )
+    ) else (
+        echo.
+		echo Extracting all RomPacks...
+        echo.
+		for %%f in ("..\ROMs Starter Pack\RetroBat Only\*.7z") do (
+            rem Check if RomPack archive has already been extracted
+            if exist "%ROMsPath%\roms\%%~nf" (
+                choice /C YN /M "RomPack %%~nxf already exists. Do you want to overwrite it"
+                if errorlevel 2 (
+                    echo Skipping extraction of %%~nxf.
+                ) else (
+                    echo Extracting %%~nxf to %ROMsPath%\roms...
+                    7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                    if errorlevel 1 (
+                        echo Error: Extraction of %%~nxf failed.
+                    ) else (
+                        echo Extraction of %%~nxf completed.
+                    )
+                )
+            ) else (
+                echo Extracting %%~nxf to %ROMsPath%\roms...
+                7z x "%%f" -o"%ROMsPath%\roms" -y > nul
+                if errorlevel 1 (
+                    echo Error: Extraction of %%~nxf failed.
+                ) else (
+                    echo Extraction of %%~nxf completed.
+                )
+            )
+        )
+    )
+) else (
+    echo No .7z archives found in 'RetroBat Only' folder.
+    echo Use the checkbox selection inside the torrent content in order to download these RomPacks.
+)
+
+:END
 echo.
 echo All done, enjoy!
 echo.
